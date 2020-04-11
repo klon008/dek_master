7570c75f400afcd1b65bc8fe65564a13a34a553f
klon_008 <klon_008@mail.ru>
2020-04-12 01:57:20 +0700

    завершил все блоки, первамя модалка

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
-import jQuery from 'jquery';
+import $ from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
 
-jQuery(function() {
-    
+$(document).ready(function(){
+	$("#vanbarMainTop").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 });
